059b8f2efb7197429e9158735da32a08b5ebd312
andyneilson42 <andy.neilson.42@gmail.com>
2021-02-20 10:24:38 -0500

    Formatting; remove config from sign and reference config in context instead.

--- src/main/scala/andy42/ssc/TWStream.scala
+++ src/main/scala/andy42/ssc/TWStream.scala
@@ -23,31 +23,32 @@ import scala.concurrent.ExecutionContext.global
 object TWStream {
 
   /** Create a Stream of io.circe.Json from the Twitter sample stream. */
-  def jsonStream[F[_] : ConcurrentEffect : ContextShift]: Reader[Config, Stream[F, Json]] = Reader { config =>
+  def jsonStream[F[_] : ConcurrentEffect : ContextShift]: Reader[Config, Stream[F, Json]] =
+    Reader { config =>
 
-    // jawn-fs2 needs to know that we want the JSON parsed to io.circe.Json
-    implicit val f: Facade[Json] = new io.circe.jawn.CirceSupportParser(None, allowDuplicateKeys = false).facade
+      // jawn-fs2 needs to know that we want the JSON parsed to io.circe.Json
+      implicit val f: Facade[Json] = new io.circe.jawn.CirceSupportParser(None, allowDuplicateKeys = false).facade
 
-    /** Sign the request with OAuth 1.0a API key and access token.
-      * OAuth signing is an effect due to generating a nonce for each `Request`.
-      */
-    def sign(req: Request[F], config: Config): F[Request[F]] = {
-      val consumer = oauth1.Consumer(config.twitterStream.apiKey, config.twitterStream.apiKeySecret)
-      val token = oauth1.Token(config.twitterStream.accessToken, config.twitterStream.accessTokenSecret)
-      oauth1.signRequest(req, consumer, callback = None, verifier = None, token = Some(token))
+      /** Sign the request with OAuth 1.0a API key and access token.
+        * OAuth signing is an effect due to generating a nonce for each `Request`.
+        */
+      def sign(req: Request[F]): F[Request[F]] = {
+        val consumer = oauth1.Consumer(config.twitterStream.apiKey, config.twitterStream.apiKeySecret)
+        val token = oauth1.Token(config.twitterStream.accessToken, config.twitterStream.accessTokenSecret)
+        oauth1.signRequest(req, consumer, callback = None, verifier = None, token = Some(token))
+      }
+
+      for {
+        // Create an HTTP client
+        client <- BlazeClientBuilder(global).stream
+
+        // Create a `Request[F]` and sign it with OAuth credentials
+        request = Request[F](Method.GET, uri = uri"https://stream.twitter.com/1.1/statuses/sample.json")
+        // `sign` returns a `F`, so we need to `Stream.eval` it to use a for-comprehension.
+        signedRequest <- Stream.eval(sign(request))
+
+        // Parse the response stream using jawn, producing a Stream of io.circe.Json
+        res <- client.stream(signedRequest).flatMap(_.body.chunks.parseJsonStream)
+      } yield res
     }
-
-    for {
-      // Create an HTTP client
-      client <- BlazeClientBuilder(global).stream
-
-      // Create a `Request[F]` and sign it with OAuth credentials
-      request = Request[F](Method.GET, uri = uri"https://stream.twitter.com/1.1/statuses/sample.json")
-      // `sign` returns a `F`, so we need to `Stream.eval` it to use a for-comprehension.
-      signedRequest <- Stream.eval(sign(request, config))
-
-      // Parse the response stream using jawn, producing a Stream of io.circe.Json
-      res <- client.stream(signedRequest).flatMap(_.body.chunks.parseJsonStream)
-    } yield res
-  }
 }
