18df0873c717f36f24bb94065c6693b309f1cf7d
andyneilson42 <andy.neilson.42@gmail.com>
2021-02-20 10:00:19 -0500

    Formatting

--- src/main/scala/andy42/ssc/StreamProcessing.scala
+++ src/main/scala/andy42/ssc/StreamProcessing.scala
@@ -9,16 +9,21 @@ import io.circe.Json
 
 object StreamProcessing {
 
-  def decodeToTweetExtract[F[_] : Concurrent](initialStream: Stream[F, Json]): Reader[Config, Stream[F, TweetExtract]] =
+  def decodeToTweetExtract[F[_] : Concurrent](initialStream: Stream[F, Json])
+  : Reader[Config, Stream[F, TweetExtract]] =
     Reader { config: Config =>
 
       // A configured instance of the tweet decoder
-      val decodeToEither: Json => Either[String, TweetExtract] = TweetExtract.decode(EventTime(config.eventTime))
+      val decodeToEither: Json => Either[String, TweetExtract] =
+        TweetExtract.decode(EventTime(config.eventTime))
 
       // Wrap the decoding in a Concurrent effect, and convert the result of conversion from an Either to a pure Stream.
-      val decodeToPureStreamInCurrentEffect: Json => F[Stream[Pure, TweetExtract]] = (json: Json) =>
-        Concurrent[F].pure(decodeToEither(json)
-          .fold(_ => Stream.empty, (tweetExtract: TweetExtract) => Stream.emit(tweetExtract)))
+      val decodeToPureStreamInConcurrentEffect: Json => F[Stream[Pure, TweetExtract]] =
+        (json: Json) =>
+          Concurrent[F].pure {
+            decodeToEither(json)
+              .fold(_ => Stream.empty, Stream.emit)
+          }
 
       val maxConcurrent = config.streamParameters.extractConcurrency
 
@@ -26,27 +31,28 @@ object StreamProcessing {
       // The presumption is that this stage will use significant CPU, so we can increase the concurrency
       // to use available cores to increase overall throughput.
       initialStream
-        .parEvalMapUnordered(maxConcurrent)(decodeToPureStreamInCurrentEffect)
+        .parEvalMapUnordered(maxConcurrent)(decodeToPureStreamInConcurrentEffect)
         .flatten
     }
 
   def summarizeStream[F[_] : Clock : Timer : Concurrent](extractedTweetStream: Stream[F, TweetExtract])
-  : Reader[Config, Stream[F, WindowSummaryOutput]] = Reader { config =>
+  : Reader[Config, Stream[F, WindowSummaryOutput]] =
+    Reader { config =>
 
-    // Combines a chunk of tweets into a WindowSummaries.
-    // This uses a Clock to determine when windows are expired.
-    val summaryCombiner:
-      (WindowSummaries, Chunk[TweetExtract]) => F[(WindowSummaries, Stream[Pure, WindowSummaryOutput])] =
-      WindowSummaries.configuredChunkedTweetCombiner[F](Clock[F], config)
+      // Combines a chunk of tweets into a WindowSummaries.
+      // This uses a Clock to determine when windows are expired.
+      val summaryCombiner:
+        (WindowSummaries, Chunk[TweetExtract]) => F[(WindowSummaries, Stream[Pure, WindowSummaryOutput])] =
+        WindowSummaries.configuredChunkedTweetCombiner[F](Clock[F], config)
 
-    // Group in chunks as large as possible, but put an upper limit on how long we will wait before emitting.
-    extractedTweetStream
-      .groupWithin(n = config.streamParameters.chunkSizeLimit, d = config.streamParameters.chunkGroupTimeout)
+      // Group in chunks as large as possible, but put an upper limit on how long we will wait before emitting.
+      extractedTweetStream
+        .groupWithin(n = config.streamParameters.chunkSizeLimit, d = config.streamParameters.chunkGroupTimeout)
 
-      // Aggregate tweet extracts in windows, and emit them as windows expire.
-      .evalScan((WindowSummaries(), Stream.emits(Seq.empty[WindowSummaryOutput]))) {
-        case ((windowSummaries, _), tweetExtractChunk) => summaryCombiner(windowSummaries, tweetExtractChunk)
-      }
-      .flatMap(_._2)
-  }
+        // Aggregate tweet extracts in windows, and emit them as windows expire.
+        .evalScan((WindowSummaries(), Stream.emits(Seq.empty[WindowSummaryOutput]))) {
+          case ((windowSummaries, _), tweetExtractChunk) => summaryCombiner(windowSummaries, tweetExtractChunk)
+        }
+        .flatMap(_._2)
+    }
 }
